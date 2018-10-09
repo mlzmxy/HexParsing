26d02b0b212d43f787e7efbd2dc6ffc235a6278d
mlzmxy <mlzmxy@139.com>
2018-10-09 23:26:29 +0800

    update

--- hexparsing.cpp
+++ hexparsing.cpp
@@ -1,6 +1,40 @@
+#include <iftream>
+#include <string>
+#include <vector>
+
 #include "hexparsing.h"
+
+const unsigned short int fill_value = 0xFFFF;  //以全1初始化数据表
 
 HexParsing::HexParsing()
 {
 
+}
+
+HexParsing::HexParsing(string file_path_name, unsigned int origin_address,
+                       unsigned int data_length)
+    :file(file_path_name),
+     addr(origin_address),
+     len(data_length)
+{
+    //设置数据表大小，并全部初始化为1
+    map.resize(data_length*1024, fill_value);
+    error_code = 0;
+}
+
+bool HexParsing::Convert()
+{
+    char buf[139];
+    iftream in_file(file);
+    if(!in_file.is_open()) {
+        error_code = FILE_OPEN_ERROR;
+        return 0;
+    }
+
+    in_file.getline(buf, 139);
+    while(!in_file.eof()) {
+        
+        
+        in_file.getline(buf, 139);
+    }
 }
--- hexparsing.h
+++ hexparsing.h
@@ -5,6 +5,22 @@ class HexParsing
 {
 public:
     HexParsing();
+    HexParsing(string file_path_name, unsigned int origin_address,
+               unsigned int data_length);
+    ~HexParsing();
+
+    bool Convert();
+    vector<unsigned short int> GetDataMap();
+    string GetErrorMsg();
+    
+private:
+    vector<unsigned short int> map;  //转换后的数据表
+    string file;  //源文件
+    unsigned int addr;  //起始地址
+    unsigned int len;  //文件大小，单位为K
+    unsigned int error_code;  //错误类型
 };
+
+#define FILE_OPEN_ERROR 0x01
 
 #endif // HEXPARSING_H
