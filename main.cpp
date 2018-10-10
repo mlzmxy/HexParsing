5cf123bde8460df3df693b930234cd521c4f73bb
mlzmxy <mlzmxy@139.com>
2018-10-10 21:24:01 +0800

    v1.0 release

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
+
+#include "HexParsing.h"
+
+using namespace std;
 
 int main()
 {
-    return 0;
+	HexParsing hp("D:\\WorkSpace\\TestHexParsing\\test.hex", 0x320000, 4 * 32 * 1024);
+	if (hp.Convert())
+	{
+		cout << "转换成功！" << endl;
+		vector<unsigned short int> result = hp.GetDataMap();
+	}
+	else
+	{
+		cout << hp.GetErrorMsg().c_str() << endl;
+	}
+	system("pause");
 }
